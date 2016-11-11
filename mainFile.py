4032d910cb434adb7308ba711442914599757341
William Smith <liamcs98@gmail.com>
2016-11-11 17:11:19 -0500

    Adding a funtion to make a results file

--- mainFile.py
+++ mainFile.py
@@ -1,5 +1,21 @@
-#xalkjaslkjdlaksjdlkasjdlkasjlkdjaslkdjsalkjd
+#William Smith
+#Goal: Use ELO math to rank images in a directory
+#
+#
+from PIL import Image, ImageFilter
+import os
+import shutil
+
+
+def makeResultsFile():
+	with open("Results.txt", "w") as f:
+		for filename in os.listdir():
+			f.write(filename + '\n')
+
+
+
+
 
 if __name__ == '__main__':
-	print("MAKE AMERICA GIT AGAIN")
-	print("Things")
+	makeResultsFile()
+	
