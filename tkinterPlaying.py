4c4b3a632235f503bc2d5a8a68f5f1eb069d3f12
William Smith <liamcs98@gmail.com>
2016-11-14 17:43:35 -0500

    Playing with TKinter

--- tkinterPlaying.py
+++ tkinterPlaying.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+'''
 root = Tk()
 topframe = Frame(root)
 topframe.pack()
@@ -15,5 +15,25 @@ button1.pack(side=LEFT)
 button2.pack(side=LEFT)
 button3.pack(side=LEFT)
 button4.pack()
+
+root.mainloop()
+'''
+
+
+root = Tk()
+
+labelOne = Label(root, text="Name")
+labelTwo = Label(root, text="Password")
+entryOne = Entry(root)
+entryTwo = Entry(root)
+
+labelOne.grid(row=0, sticky=E)
+labelTwo.grid(row=1, sticky=E)
+
+entryOne.grid(row=0, column=1)
+entryTwo.grid(row=1, column=1)
+
+checkbox = Checkbutton(root, text="Keep me logged in")
+checkbox.grid(row=3, columnspan=2)
 
 root.mainloop()
