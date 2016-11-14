c00bf8121fc3342a0364f669f43bc3852005d7aa
William Smith <liamcs98@gmail.com>
2016-11-14 17:56:43 -0500

    Playing with TKinter

--- tkinterPlaying.py
+++ tkinterPlaying.py
@@ -19,7 +19,7 @@ button4.pack()
 root.mainloop()
 '''
 
-
+'''
 root = Tk()
 
 labelOne = Label(root, text="Name")
@@ -37,3 +37,38 @@ checkbox = Checkbutton(root, text="Keep me logged in")
 checkbox.grid(row=3, columnspan=2)
 
 root.mainloop()
+'''
+'''
+root = Tk()
+
+def printName(event):
+	print("Hi, MAKE AMERICA GREAT AGAIN")
+
+#button_1 = Button(root, text="America", command=printName)
+button_1 = Button(root, text="America")
+button_1.bind("<Button-1>", printName)
+button_1.pack()
+
+root.mainloop()
+'''
+'''
+root = Tk()
+
+def leftClick(event):
+	print("left")
+
+def rightClick(event):
+	print("right")
+def middleClick():
+	print("Middle!!!")
+
+frame = Frame(root, width=300, height=250)
+frame.bind("<Button-1>", leftClick)
+frame.bind("<Button-2>", middleClick)
+frame.bind("<Button-3>", rightClick)
+
+frame.pack()
+
+root.mainloop()
+'''
+
